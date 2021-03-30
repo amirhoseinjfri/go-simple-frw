9fa63717cdda2a0be0436d8204170b3c4c70f04c
Amirhosein Jafari Barani <amirhoseinjaafari@hotmail.com>
2021-03-30 12:15:07 +0430

    fixed simpified to X Bug

--- file.go
+++ file.go
@@ -15,7 +15,7 @@ func (f *FileData) ReadFile(path *os.File) FileData {
 	bio_scanner := bufio.NewScanner(path)
 	for bio_scanner.Scan() {
 		data := bio_scanner.Text()
-		*&f.filedata = append(*&f.filedata, data)
+		f.filedata = append(f.filedata, data)
 	}
 	return *f
 }
